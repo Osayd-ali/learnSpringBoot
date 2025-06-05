dc7d46cc8bbe01ff4f88c15365f60f8354f71594
Mir Osayd Ali <mali123@depaul.edu>
2025-06-05 13:45:26 -0500

    Creating a currency service configuration

---
+++ src/main/java/com/spring/learnSpringBoot/CurrencyServiceConfiguration.java
@@ -0,0 +1,38 @@
+package com.spring.learnSpringBoot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+//currency-service.url=
+//currency-service.username=
+//currency-service.key=
+//Creating a configuration class for CurrencyService
+@ConfigurationProperties(prefix = "currency-service")
+public class CurrencyServiceConfiguration{
+    private String url;
+    private String username;
+    private String key;
+
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    @Override
+    public String toString() {
+        return "CurrencyServiceConfiguration [url=" + url + '\'' + ", username=" + username + '\'' + ", key=" + key + "]";
+    }
+}
